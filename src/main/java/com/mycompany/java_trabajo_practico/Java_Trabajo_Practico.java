e1e551af94a3c6fe22c2506bc9869d1871dbacae
usuario <usuario@DESKTOP-T9GQOKV>
2023-04-01 17:49:49 -0300

    Primer commit

---
+++ src/main/java/com/mycompany/java_trabajo_practico/Java_Trabajo_Practico.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.java_trabajo_practico;
+
+/**
+ *
+ * @author usuario
+ */
+public class Java_Trabajo_Practico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
